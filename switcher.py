ff9120f7aed19f549015f37ef857f753c1a5a621
Chris Peragine <cperagine@gmail.com>
2018-06-26 14:53:48 -0400

    Move to Correct Dir

--- switcher.py
+++ switcher.py
@@ -2,9 +2,19 @@
 import sys
 import os
 import argparse
+import subprocess
+
+# Globals
+PHPINI = "/etc/apache2/"
+FILEINI = "httpd.conf"
 
 def main():
-    print("test")
+    #Relocate to correct directory
+    changeDir()
+
+def changeDir(dir=PHPINI):
+    os.chdir(dir)
+    # print(os.getcwd())
 
 if __name__ == "__main__":
     sys.exit(main())
