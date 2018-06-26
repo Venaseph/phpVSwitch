ee8baf9216da1aa328af0c2b6becc3a2dba53dc3
Chris Peragine <cperagine@gmail.com>
2018-06-26 15:11:15 -0400

    readFile Working

--- switcher.py
+++ switcher.py
@@ -6,18 +6,33 @@ import argparse
 
 # globalConsts
 PHPINI = "/etc/apache2/"
-FILEINI = "httpd.conf"
+FILEINI = "testhttpd.conf"
 
 
 def main():
     #Relocate to correct directory
     changeDir()
+    iniFile = readFile()
 
 
+# Switch to correct directory of file
 def changeDir(dir=PHPINI):
     os.chdir(dir)
     # print(os.getcwd())
 
+#Read file into Memory
+def readFile(iniFile=FILEINI):
+    try:
+        with open(iniFile, 'r') as file:
+            iniFile = file.read()
+            print(iniFile)
+            return iniFile
+    except Exception as ex:
+        errorPrint(ex)
+
+# Method to handle excepts
+def errorPrint(ex):
+    print("Error: ", ex)
 
 if __name__ == "__main__":
     sys.exit(main())
