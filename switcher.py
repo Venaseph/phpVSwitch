461d5a71b9aadffc2943e41cdeb4a3d8ca6a4411
Chris Peragine <cperagine@gmail.com>
2018-06-26 15:47:10 -0400

    switcherFunctional

--- switcher.py
+++ switcher.py
@@ -1,34 +1,52 @@
 # !/usr/bin/env python
 import sys
 import os
-import argparse
 
 
 # globalConsts
-PHPINI = "/etc/apache2/"
-FILEINI = "testhttpd.conf"
+CONFDIR = "/etc/apache2/"
+CONFFILE = "testhttpd.conf"
 
 
 def main():
     #Relocate to correct directory
     changeDir()
-    iniFile = readFile()
+    confFile = readFile()
+    switcher(confFile)
+
+
+# Find and update version in php.ini
+def switcher(confFile):
+    print("Starting Values")
+    print(confFile[175])
+    print(confFile[176])
+    if '#' not in confFile[175]:
+        confFile[175] = "#" + confFile[175]
+        confFile[176] = confFile[176][1:]
+    else:
+        confFile[175] = confFile[175][1:]
+        confFile[176] = "#" + confFile[176]
+    print("Ending Values")
+    print(confFile[175])
+    print(confFile[176])
+    
 
 
 # Switch to correct directory of file
-def changeDir(dir=PHPINI):
+def changeDir(dir=CONFDIR):
     os.chdir(dir)
     # print(os.getcwd())
 
+
 #Read file into Memory
-def readFile(iniFile=FILEINI):
+def readFile(confFile=CONFFILE):
     try:
-        with open(iniFile, 'r') as file:
-            iniFile = file.read()
-            print(iniFile)
-            return iniFile
+        with open(confFile, 'r') as file:
+            file = file.read().splitlines()
+            return file
     except Exception as ex:
         errorPrint(ex)
+
 
 # Method to handle excepts
 def errorPrint(ex):
