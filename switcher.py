a7c00ba39f6a4a3899c9f1e72e764b0befcc3438
Chris Peragine <cperagine@gmail.com>
2018-06-26 14:55:15 -0400

    Refact

--- switcher.py
+++ switcher.py
@@ -2,19 +2,22 @@
 import sys
 import os
 import argparse
-import subprocess
 
-# Globals
+
+# globalConsts
 PHPINI = "/etc/apache2/"
 FILEINI = "httpd.conf"
+
 
 def main():
     #Relocate to correct directory
     changeDir()
 
+
 def changeDir(dir=PHPINI):
     os.chdir(dir)
     # print(os.getcwd())
+
 
 if __name__ == "__main__":
     sys.exit(main())
