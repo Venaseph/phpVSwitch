c7fea76ddbaf6d840601d6f94c78b9d59972dfef
Chris Peragine <cperagine@gmail.com>
2018-06-26 16:09:32 -0400

    changes to flow, working on readFile

--- switcher.py
+++ switcher.py
@@ -11,37 +11,56 @@ CONFFILE = "testhttpd.conf"
 def main():
     #Relocate to correct directory
     changeDir()
+
+    # Read in current httpd.conf file
     confFile = readFile()
+
+    # Adjust list to reflect correct PHP Version
     switcher(confFile)
+
+    # Write-to file
+    writeFile(confFile)
+
+
+# Write-to httpd.conf
+def writeFile(confFile):
+    pass
+
 
 
 # Find and update version in php.ini
 def switcher(confFile):
-    print("Starting Values")
-    print(confFile[175])
-    print(confFile[176])
+    # print("Starting Values")
+    # print(confFile[175])
+    # print(confFile[176])
     if '#' not in confFile[175]:
         confFile[175] = "#" + confFile[175]
         confFile[176] = confFile[176][1:]
     else:
         confFile[175] = confFile[175][1:]
         confFile[176] = "#" + confFile[176]
-    print("Ending Values")
-    print(confFile[175])
-    print(confFile[176])
-    
+    # print("Ending Values")
+    # print(confFile[175])
+    # print(confFile[176])
 
 
 # Switch to correct directory of file
 def changeDir(dir=CONFDIR):
-    os.chdir(dir)
-    # print(os.getcwd())
+    try:
+        os.chdir(dir)
+    except Exception as ex:
+        errorPrint(ex)
 
 
-#Read file into Memory
+# Read file into Memory
 def readFile(confFile=CONFFILE):
     try:
         with open(confFile, 'r') as file:
+            for i, line in enumerate(file):
+                if i == 175:
+                    print(line)
+                elif i ==176:
+                    print(line)
             file = file.read().splitlines()
             return file
     except Exception as ex:
